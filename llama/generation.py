2c17ed8af88c21372177510456e074e0144394dc
thinkingface <67254656+zzazanz@users.noreply.github.com>
2024-06-16 20:55:01 +0900

    change two values to tuple

--- llama/generation.py
+++ llama/generation.py
@@ -258,7 +258,7 @@ class Llama:
         if max_gen_len is None:
             max_gen_len = self.model.params.max_seq_len - 1
         prompt_tokens = [self.tokenizer.encode(x, bos=True, eos=False) for x in prompts]
-        generation_tokens, generation_logprobs, attn_list, mlp_list = self.generate(
+        (generation_tokens, generation_logprobs), attn_list, mlp_list = self.generate(
             prompt_tokens=prompt_tokens,
             max_gen_len=max_gen_len,
             temperature=temperature,
